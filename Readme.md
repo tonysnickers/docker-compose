bab8d8d5a67d97c43fff34394a2d9be38ed0f53a
tonysnickers <antoine.mendy@hotmail.fr>
2023-04-14 14:46:38 +0200

    docker compose

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Start the projet run this command
+
+
+`docker compose -f docker-compose.dev.yml up --build`
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>helloo</h1>
+    </div>
+  )
+}
+
+export default App
